bcdc2e357576b7c745f73d22a0570fa0e83cef5f
Alex Sayers <alex@asayers.com>
2025-10-02 09:58:49 +0900

    Switch from clap to bpaf

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
+use bpaf::Bpaf;
 use bytesize::ByteSize;
-use clap::Parser;
 use sha1::Digest;
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
 
-#[derive(Parser)]
+#[derive(Bpaf)]
+#[bpaf(options, fallback_to_usage)]
 struct Opts {
+    #[bpaf(positional)]
     file: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
-    let opts = Opts::parse();
+    let opts = opts().run();
     let archive = tar::Archive::new(File::open(opts.file)?);
     for (hdr, mut slice) in fileslice::slice_tarball(archive)? {
         if hdr.entry_type() == tar::EntryType::Regular {
